aab6f6d309fb398a84a0d639f18370d18b5236fa
lechwal <30338730+lechwal@users.noreply.github.com>
2017-08-10 18:41:13 +0200

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,57 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+
+// Http import
+import { HttpModule } from '@angular/http';
+
+// Page imports
+import { CategoryPage } from '../pages/category/category';
+import { ListPage } from '../pages/list/list';
+import { HomePage } from '../pages/home/home';
+import { TabsPage } from '../pages/tabs/tabs';
+import { SingleItem } from '../pages/single-item/single-item';
+import { cPenalPage } from '../pages/cPenal/cPenal';
+
+// Service imports
+import { ItemApi } from '../services/item-api.service';
+
+// Native imports
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    CategoryPage,
+    ListPage,
+    HomePage,
+    SingleItem,
+    TabsPage,
+    cPenalPage
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    CategoryPage,
+    ListPage,
+    HomePage,
+    SingleItem,
+    TabsPage,
+    cPenalPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    ItemApi,
+    HttpModule,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
---
+++ src/pages/home/home.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+import { Http } from '@angular/http';
+
+// Import pages to allow links to the page
+import { cPenalPage } from '../../pages/cPenal/cPenal';
+
+// Service import for items
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html',
+  providers: [Http]
+})
+export class HomePage {
+
+  // The items array to populate with data is created
+  items: any;
+
+  constructor(
+              public navCtrl: NavController,
+              public params:NavParams,
+            )
+            {}
+
+  // This is where the data loads from the service.
+  // It happens when the view loads for the first time.
+ 
+
+  navigateTo(){
+    this.navCtrl.push(cPenalPage)
+  }
+
+}
